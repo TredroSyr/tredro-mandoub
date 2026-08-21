c15be45339a7c6772d02d6d41d4bee762bb87cc3
safwatbilal <safwetbilal65@gmail.com>
2026-08-21 13:03:53 +0300

    trigger

--- module/auth/store/auth-store.ts
+++ module/auth/store/auth-store.ts
@@ -35,7 +35,7 @@ export const useAuthStore = create<AuthState>()(
         set({ accessToken, isAuthenticated: true }),
 
       setRefreshToken: (refreshToken) => set({ refreshToken }),
-
+      //trigger
       updateRep: (partial) =>
         set({
           rep: get().rep ? { ...get().rep!, ...partial } : null,
