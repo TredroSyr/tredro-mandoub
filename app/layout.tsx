0b116c62c9146ed48c56cf62a52f1163588ad7d0
safwatbilal <safwetbilal65@gmail.com>
2026-09-12 15:13:07 +0300

    chore: comment out ErudaLoader component in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import "./globals.css";
 import "leaflet/dist/leaflet.css";
 import { Toaster } from "@/components/ui/toast";
 import { QueryProvider } from "@/components/provider/QueryClientProvider";
-import ErudaLoader from "@/components/tredro/ErudaLoader";
+
 import HardwareBackButton from "@/components/tredro/HardwareBackButton";
 import UpdateChecker from "@/components/tredro/UpdateChecker";
 
@@ -92,7 +92,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
           <AppDownloadDrawer />
           <UpdateChecker />
 
-          <ErudaLoader />
+          {/*  <ErudaLoader />*/}
           <Suspense fallback={null}>
             <HardwareBackButton />
           </Suspense>
